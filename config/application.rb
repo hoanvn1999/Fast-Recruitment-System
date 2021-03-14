2739287b6c21a866a441c91e03872ac8a4059c06
hoanvn1999 <hoanvn1999@gmail.com>
2021-03-14 11:05:34 +0700

    Homepage

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module FastHiring
+module FastRequirement
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :en
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
-
+    root "homepage#index"
   end
 end
