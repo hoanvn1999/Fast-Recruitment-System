3ec0673b6cc98ba7cfbc783c83df10dfcce56931
hoanvn1999 <hoanvn1999@gmail.com>
2021-03-19 09:34:23 +0700

    Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   belongs_to :institution
   has_many :jobs
   has_many :curriculum_vitaes
@@ -23,4 +24,64 @@ class User < ApplicationRecord
   enum role: {recruiter: 0, candidate: 1, admin: 2}
 
   has_secure_password
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             Engine::MIN_COST
+           else
+             Engine.cost
+           end
+    Password.create string, cost: cost
+  end
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false unless digest
+
+    Password.new(digest).is_password? token
+  end
+
+  def forget
+    update remember_digest: nil
+  end
+
+  def active
+    update activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest: User.digest(reset_token),
+           reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
