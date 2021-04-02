36633ef2e819a13e078e9de68fde41ccd9391521
hoanvn1999 <hoanvn1999@gmail.com>
2021-04-03 02:32:34 +0700

    Apply and Close job_Candidate

---
+++ app/controllers/candidate/recruitments_controller.rb
@@ -0,0 +1,50 @@
+class Candidate::RecruitmentsController < CandidateController
+  before_action :new_recruitmenr, only: :create
+
+  def create
+    respond_to do |format|
+      if !logged_in?
+        format.html do
+          flash[:warning] = t "account.pls_login"
+          redirect_to login_path
+        end
+      elsif cv_id.blank?
+        # Chuyen sang trang tao CV va thong bao chua tao CV thuoc linh vuc nay
+      elsif @recruitment.save
+        format.js {}
+      else
+        format.html do
+          flash[:warning] = t "apply.fail"
+          redirect_to job_path(id: params[:job_id])
+        end
+      end
+    end
+  end
+
+  def destroy
+    recruitment = Recruitment.find_by(curriculum_vitae_id: params[:cv_id],
+                                      job_id: params[:job_id])
+    respond_to do |format|
+      if recruitment.destroy
+        format.js {}
+      else
+        format.html do
+          flash[:warning] = t "job.update_fail"
+          redirect_to candidate_curriculum_vitae_applied_path
+        end
+      end
+    end
+  end
+
+  private
+
+  def cv_id
+    job = Job.find_by id: params[:job_id]
+    current_user.curriculum_vitaes.where(field_id: job.field_id).ids
+  end
+
+  def new_recruitmenr
+    @recruitment = Recruitment.new job_id: params[:job_id],
+                                  curriculum_vitae_id: cv_id
+  end
+end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,12 +1,8 @@
 class JobsController < ApplicationController
-  def index
-    @jobs = Job.field(params[:field_id])
-               .type(params[:type_of_work])
-               .arrange_salary(params[:salary])
-               .experience(params[:candidate_experience])
-               .paginate(page: params[:page],
-                         per_page: Settings.per_page.default)
-  end
+  before_action :new_recruitment, only: :apply
+  before_action :get_all_jobs, except: :show
+
+  def index; end
 
   def show
     @job = Job.find_by id: params[:id]
@@ -15,5 +11,89 @@ class JobsController < ApplicationController
 
     flash[:danger] = t "job.nil"
     redirect_to root_path
+  end
+
+  def apply
+    respond_to do |format|
+      if !logged_in?
+        format.html do
+          apply_not_login
+        end
+      elsif cv_id.blank?
+        # Chuyen sang trang tao CV va thong bao chua tao CV thuoc linh vuc nay
+      elsif @recruitment.save
+        format.js {}
+        format.html do
+          apply_save
+        end
+      else
+        format.html do
+          apply_fail
+        end
+      end
+    end
+  end
+
+  def close
+    recruitment = Recruitment.find_by(curriculum_vitae_id: params[:cv_id],
+                                      job_id: params[:id])
+    respond_to do |format|
+      if recruitment.destroy
+        format.js {}
+        format.html do
+          close_success
+        end
+      else
+        format.html do
+          close_fail
+        end
+      end
+    end
+  end
+
+  private
+
+  def get_all_jobs
+    @jobs = Job.field(params[:field_id])
+               .type(params[:type_of_work])
+               .arrange_salary(params[:salary])
+               .experience(params[:candidate_experience])
+               .paginate(page: params[:page],
+                         per_page: Settings.per_page.default)
+  end
+
+  def cv_id
+    job = Job.find_by id: params[:id]
+    current_user.curriculum_vitaes.where(field_id: job.field_id).ids.first
+  end
+
+  def new_recruitment
+    @recruitment = Recruitment.new job_id: params[:id],
+                                   curriculum_vitae_id: cv_id
+  end
+
+  def apply_not_login
+    flash[:warning] = t "account.pls_login"
+    redirect_to login_path
+  end
+
+  def apply_save
+    flash[:success] = t "apply.success"
+    redirect_to job_path(id: params[:id])
+  end
+
+  def apply_fail
+    flash[:warning] = t "apply.fail"
+    redirect_to job_path(id: params[:job_id])
+  end
+
+  def close_success
+    flash[:success] = t "job.close_applied_job"
+    redirect_to job_path(id: params[:id])
+  end
+
+  def close_fail
+    flash[:warning] = t "job.update_fail"
+    redirect_to job_path(id: params[:id])
   end
 end
